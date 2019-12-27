a8e1b7fd5769177f7ee770868bd7c87e35684b76
Megan Venetianer <megan.venetianer@gmail.com>
2019-12-26 19:40:46 -0500

    Updated author and book files

--- author.js
+++ author.js
@@ -1,5 +1,15 @@
 class Author {
+  constructor(bookAuthor) {
+    this.first = bookAuthor.first;
+    this.last = bookAuthor.last;
+    this.books = [];
+  }
+  write(title, publicationYear) {
 
+  }
+  instanceOf() {
+
+  }
 }
 
 module.exports = Author;
