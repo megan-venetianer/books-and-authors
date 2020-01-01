5555cd23effcebddce1c7086d53e3ea6f69ecce9
Megan Venetianer <megan.venetianer@gmail.com>
2020-01-01 13:12:34 -0700

    Updated author.js

--- author.js
+++ author.js
@@ -1,15 +1,16 @@
+var Book = require('./book.js');
+
 class Author {
   constructor(bookAuthor) {
     this.first = bookAuthor.first;
     this.last = bookAuthor.last;
     this.books = [];
   }
-  write() {
-
+  write(title, date) {
+    var newBook = new Book(this.first, this.last, title, date);
+    this.books.push(newBook);
   }
-  instanceOf() {
+};
 
-  }
-}
 
 module.exports = Author;
